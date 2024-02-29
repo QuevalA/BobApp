0d61c8e2f5681d390ee302791850e878597631fd
QuevalA <87530892+QuevalA@users.noreply.github.com>
2024-02-29 17:05:36 +0100

    [Debug] Angular tests workflow error.

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,6 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {JokesService} from "./services/jokes.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
